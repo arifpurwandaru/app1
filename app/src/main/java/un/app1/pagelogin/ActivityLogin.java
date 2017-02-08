f7e745bad9a5b169e1e4d4912db749c39c7e5f57
fajar dian prasetya <drive24f@gmail.com>
2017-02-08 18:10:52 +0700

    Initial commit

--- app/src/main/java/un/app1/pagelogin/ActivityLogin.java
+++ app/src/main/java/un/app1/pagelogin/ActivityLogin.java
@@ -3,14 +3,24 @@ package un.app1.pagelogin;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import javax.inject.Inject;
+
 import un.app1.MainApp;
 import un.app1.R;
 import un.app1.databinding.ActivityLoginBinding;
 import un.app1.network.internet.ConnectivityReceiver;
+import un.app1.network.service.MainService;
+import un.app1.network.service.RetBuilder;
 
-public class ActivityLogin extends AppCompatActivity implements ConnectivityReceiver.ConnectivityReceiverListener {
+public class ActivityLogin extends AppCompatActivity implements LoginView, ConnectivityReceiver.ConnectivityReceiverListener {
+
+    @Inject
+    public RetBuilder retBuilder;
 
     ActivityLoginBinding binding;
+    LoginPresenter presenter;
+    MainService mainService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +30,9 @@ public class ActivityLogin extends AppCompatActivity implements ConnectivityRece
         MainApp.getInstance().setConnectivityListener(ActivityLogin.this);
 
         binding = DataBindingUtil.setContentView(ActivityLogin.this, R.layout.activity_login);
-//        mainService = new MainService(retBuilder.service());
-//        homePresenter = new HomePresenter(ActivityHome.this, mainService);
-//        binding.setPresenter(homePresenter);
+        mainService = new MainService(retBuilder.service());
+        presenter = new LoginPresenter(ActivityLogin.this, mainService);
+        binding.setPresenter(presenter);
 
     }
 
